a865ab56cfe6910aa584281d235e3eab196b24b8
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-06 04:58:30 +0800

    reset code

--- examples/server_upload.rs
+++ examples/server_upload.rs
@@ -11,6 +11,7 @@ use cogo_http::server::{Request, Response};
 
 // request header Content-Type: json
 fn hello(mut req: Request, res: Response) {
+    let mut files=vec![];
     let form = read_formdata(&mut req.body, &req.headers).unwrap();
     for (k, mut v) in form.files {
         if v.path.is_file() {
@@ -19,10 +20,11 @@ fn hello(mut req: Request, res: Response) {
             if !name.is_empty() {
                 //rename
                 std::fs::rename(v.path.to_str().unwrap(), "target/mime_multipart/".to_string() + &name);
+                files.push(name);
             }
         }
     }
-    res.send(serde_json::json!(form.fields).to_string().as_bytes()).unwrap();
+    res.send(format!("upload:{:?}",files).to_string().as_bytes()).unwrap();
 }
 
 fn main() {
