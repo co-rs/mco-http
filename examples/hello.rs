0a25ac220e367e5b6b923547aa950a3c3048774e
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-05 18:14:07 +0800

    add example

--- examples/hello.rs
+++ examples/hello.rs
@@ -12,7 +12,7 @@ fn hello(_: Request, res: Response) {
 
 fn main() {
     env_logger::init().unwrap();
-    let _listening = cogo_http::Server::http("127.0.0.1:3000").unwrap()
+    let _listening = cogo_http::Server::http("0.0.0.0:3000").unwrap()
         .handle(hello);
     println!("Listening on http://127.0.0.1:3000");
 }
