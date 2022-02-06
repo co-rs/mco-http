824df4616aa1d83fa0b39c8b335768c2d983b729
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-07 01:35:23 +0800

    add download

--- examples/server_download.rs
+++ examples/server_download.rs
@@ -8,7 +8,6 @@ use std::io::{Read, Write};
 use std::ops::Deref;
 
 use std::path::PathBuf;
-use std::sync::Arc;
 use std::sync::mpsc::RecvError;
 use captcha::Captcha;
 use captcha::filters::{Dots, Noise, Wave};
@@ -18,42 +17,16 @@ use cogo::std::sync::{Receiver, Sender};
 use cogo_http::header::ContentType;
 use cogo_http::server::{Request, Response};
 
+pub static PngChannel:Lazy<(Sender<Vec<u8>>,Receiver<Vec<u8>>)> = Lazy::new(||{ chan!(1) });
+
 /// Draw a captcha code and display it on the web
 fn download(mut req: Request, res: Response) {
     //first set header content type
     res.headers.set(ContentType::png());
 
-
     //next,req thread new an png to me
-    let c = Arc::new(chan!(1));
-    //Heavy computing tasks should be performed by threads
-    let channel_clone = c.clone();
-    std::thread::spawn(move || {
-        match channel_clone.1.recv() {
-            Ok(v) => {
-                defer!(||{
-                    // make sure that messages will be sent even if panic occurs
-                    if channel_clone.0.remain() == 0 {
-                        channel_clone.0.send(vec![]);
-                    }
-                });
-                let mut captcha = Captcha::new();
-                captcha
-                    .add_chars(4)
-                    .apply_filter(Noise::new(0.1))
-                    .apply_filter(Wave::new(1.0, 10.0).horizontal())
-                    // .apply_filter(Wave::new(2.0, 20.0).vertical())
-                    .view(160, 60)
-                    .apply_filter(Dots::new(4));
-                let png = captcha.as_png().unwrap();
-                channel_clone.0.send(png);
-            }
-            Err(_) => {}
-        }
-    });
-    c.0.send(vec![]);
-    let png = c.1.recv().unwrap_or_default();
-
+    PngChannel.0.send(vec![]);
+    let png=PngChannel.1.recv().unwrap_or_default();
 
     //return data
     res.send(&png).unwrap();
@@ -61,6 +34,33 @@ fn download(mut req: Request, res: Response) {
 
 fn main() {
     env_logger::init().unwrap();
+
+    //Heavy computing tasks should be performed by threads
+    std::thread::spawn(||{
+       loop{
+           match PngChannel.1.recv(){
+               Ok(v) => {
+                   defer!(||{
+                    // make sure that messages will be sent even if panic occurs
+                    if PngChannel.0.remain() == 0 {
+                        PngChannel.0.send(vec![]);
+                    }
+                   });
+                   let mut captcha = Captcha::new();
+                   captcha
+                       .add_chars(4)
+                       .apply_filter(Noise::new(0.1))
+                       .apply_filter(Wave::new(1.0, 10.0).horizontal())
+                       // .apply_filter(Wave::new(2.0, 20.0).vertical())
+                       .view(160, 60)
+                       .apply_filter(Dots::new(4));
+                   let png = captcha.as_png().unwrap();
+                   PngChannel.0.send(png);
+               }
+               Err(_) => {}
+           }
+       }
+    });
 
     let _listening = cogo_http::Server::http("0.0.0.0:3000").unwrap()
         .handle(download).unwrap();
