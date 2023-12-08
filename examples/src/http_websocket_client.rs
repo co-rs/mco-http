f71fc1d55711fdf5ada211f93199541446d06bfe
zhuxiujia@qq.com <zhuxiujia@qq.com>
2023-12-08 21:53:49 +0800

    add websocket

--- examples/src/http_websocket_client.rs
+++ examples/src/http_websocket_client.rs
@@ -1,18 +1,29 @@
 extern crate mco_http;
 extern crate fast_log;
 
-use fast_log::config::Config;
+use std::time::Duration;
 use tungstenite::{connect, Message};
+use tungstenite::protocol::Role;
+use tungstenite::stream::MaybeTlsStream;
 
-//FIXME: use mco tcp stream
 fn main() {
-    let _ = fast_log::init(Config::new().level(log::LevelFilter::Info).console());
-
-    let (mut socket, response) =
+    let (mut s, response) =
         connect(url::Url::parse("ws://127.0.0.1:3000").unwrap()).expect("Can't connect");
-    socket.send(Message::Text("Hello WebSocket".to_string())).unwrap();
-    loop {
-        let msg = socket.read().expect("Error reading message");
-        println!("Received: {}", msg);
-    }
+
+    //replace TcpStream
+    let v = s.get_mut();
+    let stream = match v {
+        MaybeTlsStream::Plain(v) => {
+            v.try_clone().expect("clone stream fail")
+        }
+        _ => {
+            panic!("unknown stream")
+        }
+    };
+    let mco_tcp = mco::net::TcpStream::new(stream).unwrap();
+    let mut socket = tungstenite::WebSocket::from_raw_socket(mco_tcp, Role::Client, Option::from(s.get_config().clone()));
+    socket.send(Message::Text("Hello mco WebSocket".to_string())).unwrap();
+    let msg = socket.read().expect("Error reading message");
+    mco::coroutine::sleep(Duration::from_secs(1));
+    println!("Received: {}", msg);
 }
